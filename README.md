9844719b73ef7691f6a97845e719b0573cf2bb36
Benoît Claveau <benoit.claveau@izzili.com>
2017-06-26 11:31:28 +0200

    Force to load.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # qwebs-aws-api-gateway
 Qwebs routing for AWS API Gateway
+
+```js
+'use strict';
+
+let service = require("./service");
+
+const promise = require('qwebs-aws-api-gateway').Create({ dirname: __dirname, config: "./config.json" });
+
+module.exports.handler = (event, context, callback) => {
+    console.log("[EVENT]", event);
+    promise.then(qwebs => {
+        qwebs.invoke(event, context, callback).then(() => {
+        console.log("[SUCCESS]");
+    }).catch(error => {
+        console.log("[ERROR]", error);
+        callback(null, {
+            statusCode: error.statusCode || 404
+        })
+    })
+};
+```
+
+```config.json
+{
+    "routes": "./routes.json"
+}
+```
+
+```routes.json
+{
+    "services": [
+        { "name": "$info", "location": "../services/info"}
+    ],
+    "locators": [
+        { "get": "/info", "service": "$info", "method": "getInfo" }
+    ]
+}
+```
--- tests/qwebs.spec.js
+++ tests/qwebs.spec.js
@@ -10,44 +10,66 @@ const service = require('./mocks/service');
 
 describe("router", () => {
     
-    it("static path", done => {
-        return new Promise((resolve, reject) => {
-            let qwebs = new Qwebs();
-            qwebs.get("test/33", service, "dummy");
-            let event = {
-                httpMethod: "GET",
-                path: {
-                    proxy: "test/33"
-                }
-            }
-            let context = {};
-            let callback = (error, res) => {
-                expect(res.statusCode).toEqual(200);
-                expect(res.body.event.path.proxy).toEqual("test/33");
-                resolve(this);
-            }
-            qwebs.invoke(event, context, callback);
-        }).catch(fail).then(done);
-    });
+    // it("static path", done => {
+    //     return new Promise((resolve, reject) => {
+    //         let qwebs = new Qwebs();
+    //         qwebs.get("test/33", service, "dummy");
+    //         let event = {
+    //             httpMethod: "GET",
+    //             path: {
+    //                 proxy: "test/33"
+    //             }
+    //         }
+    //         let context = {};
+    //         let callback = (error, res) => {
+    //             expect(res.statusCode).toEqual(200);
+    //             expect(res.body.event.path.proxy).toEqual("test/33");
+    //             resolve(this);
+    //         }
+    //         qwebs.invoke(event, context, callback);
+    //     }).catch(fail).then(done);
+    // });
 
-    it("dynamic path", done => {
-        return new Promise((resolve, reject) => {
-            let qwebs = new Qwebs();
-            qwebs.get("test/:id", service, "dummy");
-            let event = {
-                httpMethod: "GET",
-                path: {
-                    proxy: "test/33"
+    // it("dynamic path", done => {
+    //     return new Promise((resolve, reject) => {
+    //         let qwebs = new Qwebs();
+    //         qwebs.get("test/:id", service, "dummy");
+    //         let event = {
+    //             httpMethod: "GET",
+    //             path: {
+    //                 proxy: "test/33"
+    //             }
+    //         }
+    //         let context = {};
+    //         let callback = (error, res) => {
+    //             expect(res.statusCode).toEqual(200);
+    //             expect(res.body.event.pathParameters.id).toEqual("33");
+    //             expect(res.body.event.path.proxy).toEqual("test/33");
+    //             resolve(this);
+    //         }
+    //         qwebs.invoke(event, context, callback);
+    //     }).catch(fail).then(done);
+    // });
+
+    it("config.json", done => {
+        return new Qwebs({ dirname: __dirname, config: "./config.json" }).load().then(qwebs => {;
+            return new Promise((resolve, reject) => {
+                let event = {
+                    httpMethod: "GET",
+                    path: {
+                        proxy: "dummy/33"
+                    }
                 }
-            }
-            let context = {};
-            let callback = (error, res) => {
-                expect(res.statusCode).toEqual(200);
-                expect(res.body.event.pathParameters.id).toEqual("33");
-                expect(res.body.event.path.proxy).toEqual("test/33");
-                resolve(this);
-            }
-            qwebs.invoke(event, context, callback);
+                let context = {};
+                let callback = (error, res) => {
+                    expect(res.statusCode).toEqual(200);
+                    expect(res.body.event.pathParameters.id).toEqual("33");
+                    expect(res.body.event.path.proxy).toEqual("dummy/33");
+                    resolve(this);
+                }
+                
+                qwebs.invoke(event, context, callback);
+            });
         }).catch(fail).then(done);
-    });
+    }, 10000);
 });
