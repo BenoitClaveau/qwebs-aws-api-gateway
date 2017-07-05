6337f7dc6870561d80a802a4f29cb4a5e2381d50
Benoît Claveau <benoit.claveau@izzili.com>
2017-07-05 14:54:28 +0200

    Http header in lower case

--- tests/aws-qwebs.spec.js
+++ tests/aws-qwebs.spec.js
@@ -5,71 +5,23 @@
  */
 "use strict";
 
-const Qwebs = require('../lib/aws-qwebs');
+const Qwebs = require('../lib/qwebs');
+const Client = require('qwebs').Client;
 const service = require('./mocks/service');
 
 describe("router", () => {
     
-    // it("static path", done => {
-    //     return new Promise((resolve, reject) => {
-    //         let qwebs = new Qwebs();
-    //         qwebs.get("test/33", service, "dummy");
-    //         let event = {
-    //             httpMethod: "GET",
-    //             path: {
-    //                 proxy: "test/33"
-    //             }
-    //         }
-    //         let context = {};
-    //         let callback = (error, res) => {
-    //             expect(res.statusCode).toEqual(200);
-    //             expect(res.body.event.path.proxy).toEqual("test/33");
-    //             resolve(this);
-    //         }
-    //         qwebs.invoke(event, context, callback);
-    //     }).catch(fail).then(done);
-    // });
-
-    // it("dynamic path", done => {
-    //     return new Promise((resolve, reject) => {
-    //         let qwebs = new Qwebs();
-    //         qwebs.get("test/:id", service, "dummy");
-    //         let event = {
-    //             httpMethod: "GET",
-    //             path: {
-    //                 proxy: "test/33"
-    //             }
-    //         }
-    //         let context = {};
-    //         let callback = (error, res) => {
-    //             expect(res.statusCode).toEqual(200);
-    //             expect(res.body.event.pathParameters.id).toEqual("33");
-    //             expect(res.body.event.path.proxy).toEqual("test/33");
-    //             resolve(this);
-    //         }
-    //         qwebs.invoke(event, context, callback);
-    //     }).catch(fail).then(done);
-    // });
+    beforeAll(() => {
+        Qwebs.Create({ dirname: __dirname, config: "./config.json" });
+    })
 
     it("config.json", done => {
-        return new Qwebs({ dirname: __dirname, config: "./config.json" }).load().then(qwebs => {;
-            return new Promise((resolve, reject) => {
-                let event = {
-                    httpMethod: "GET",
-                    path: {
-                        proxy: "dummy/33"
-                    }
-                }
-                let context = {};
-                let callback = (error, res) => {
-                    expect(res.statusCode).toEqual(200);
-                    expect(res.body.event.pathParameters.id).toEqual("33");
-                    expect(res.body.event.path.proxy).toEqual("dummy/33");
-                    resolve(this);
-                }
-                
-                qwebs.invoke(event, context, callback);
-            });
+        let client = new Client();
+        client.get({ url: "http://localhost:3000/dummy/33"}).then(res => {
+            expect(res.statusCode).toEqual(200);
+            let body = JSON.parse(res.body);
+            expect(body.event.pathParameters.id).toEqual("33");
+            expect(body.event.headers.host).toEqual("localhost:3000"); //header must be in lower case
         }).catch(fail).then(done);
     }, 10000);
 });
--- tests/mocks/service.js
+++ tests/mocks/service.js
@@ -4,6 +4,7 @@ module.exports.dummy = (event, context, callback) => {
     callback(null, {
         statusCode: 200,
         body: {
+            service: "dummy",
             event: event
         }
     });
